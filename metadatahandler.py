a792607083dd9946f2b1213a4d9609041fcf0bf3
Newsdee <newsdee@gmail.com>
2021-04-01 14:54:55 +0800

    + enhanced mapping logic to use a mapping lookup table with only a few special cases + fixed some English names for categories

--- metadatahandler.py
+++ metadatahandler.py
@@ -1,15 +1,79 @@
-import os
-import shutil
 import collections
-import sys
+from enum import Enum
+import os
 import platform
+import util
+import shutil
+import sys
 import xml.etree.ElementTree as etree
 from xml.dom import minidom
-import util
 
 DosGame = collections.namedtuple('DosGame',
                                  'dosname metadataname name genres publisher developer year frontPic manual desc')
 
+
+class GameGenres(Enum):
+    # the target genres we want to map to
+    UNKNOWN             = "Unknown"
+    MISC                = "Misc"
+    ACTION_ADVENTURE    = "Action-Adventure"
+    ADVENTURE_VISUAL    = "Adventure-Visual"
+    ADVENTURE_TEXT      = "Adventure-Text"
+    BEATEMUP            = "BeatEmUp"
+    FPS                 = "Gun-FPS"
+    PINBALL             = "Pinball"
+    PLATFORM            = "Platform"
+    PUZZLE              = "Puzzle"
+    RACING              = "Racing"
+    RPG                 = "RPG"
+    SIMULATION          = "Simulation"
+    SHMUP               = "ShootEmUp"
+    SPORTS              = "Sports"
+    STRATEGY_MANAGEMENT = "Strategy-Management"
+    TOOLS               = "Tools"
+
+# defines direct mappings (i.e. if match, return the value)
+GAME_MAPPER = {
+    'Action':               GameGenres.ACTION_ADVENTURE,
+    'Adventure':            GameGenres.ADVENTURE_VISUAL,
+    'App':                  GameGenres.TOOLS,
+    'Arcade':               GameGenres.MISC,
+    'Beat \'em Up':         GameGenres.BEATEMUP,
+    'Board / Party Game':   GameGenres.PUZZLE,
+    'Board':                GameGenres.PUZZLE,
+    'Cards / Tiles':        GameGenres.PUZZLE,
+    'Casino':               GameGenres.PUZZLE,
+    'Construction and Management Simulation': GameGenres.STRATEGY_MANAGEMENT,
+    'Creativity':           GameGenres.TOOLS,
+    'Driving':              GameGenres.RACING,
+    'FPS':                  GameGenres.FPS,
+    'Fighting':             GameGenres.BEATEMUP,
+    'First Person Shooter': GameGenres.FPS,
+    'Flight Simulator':     GameGenres.SIMULATION,
+    'Game Show':            GameGenres.PUZZLE,
+    'Interactive Fiction':  GameGenres.ADVENTURE_TEXT,
+    'Life Simulation':      GameGenres.MISC,
+    'Managerial':           GameGenres.STRATEGY_MANAGEMENT,
+    'Pinball':              GameGenres.PINBALL,
+    'Platform':             GameGenres.PLATFORM,
+    'Puzzle':               GameGenres.PUZZLE,
+    'RPG':                  GameGenres.RPG,
+    'Racing / Driving':     GameGenres.RACING,
+    'Racing':               GameGenres.RACING,
+    'Reference':            GameGenres.TOOLS,    
+    'Role-Playing':         GameGenres.RPG,
+    'Shooter':              GameGenres.SHMUP,
+    'Simulation':           GameGenres.SIMULATION,
+    'Sports':               GameGenres.SPORTS,
+    'Strategy':             GameGenres.STRATEGY_MANAGEMENT,
+    'Vehicle Simulation':   GameGenres.SIMULATION,
+    'Visual Novel':         GameGenres.ADVENTURE_VISUAL,
+}    
+   # unique, sort, get first then apply special
+   
+
+         
+         
 
 # Metadata exporting
 class MetadataHandler:
@@ -134,56 +198,30 @@ class MetadataHandler:
 
     # Convert multi genres exo collection format to a single one
     def buildGenre(self, dosGame):
-        if dosGame is not None and dosGame.genres is not None:
-            if 'Flight Simulator' in dosGame.genres or 'Vehicle Simulation' in dosGame.genres:
-                return 'Simulation'
-            elif "Education" in dosGame.genres or "Quiz" in dosGame.genres:
-                if "Adventure" in dosGame.genres or "Visual Novel" in dosGame.genres:
-                    return "Adventure-Visual"
-                else:
-                    return 'Misc'
-            elif "Racing" in dosGame.genres or "Driving" in dosGame.genres or "Racing / Driving" in dosGame.genres:
-                return "Race"
-            elif 'Sports' in dosGame.genres:
-                return 'Sports'
-            elif 'Pinball' in dosGame.genres:
-                return 'Pinball'
-            elif "Puzzle" in dosGame.genres or "Board" in dosGame.genres or "Board / Party Game" in dosGame.genres \
-                    or "Casino" in dosGame.genres or 'Cards / Tiles' in dosGame.genres or 'Game Show' in dosGame.genres:
-                return "Puzzle"
-            elif 'Shooter' in dosGame.genres:
-                return 'ShootEmUp'
-            elif 'Platform' in dosGame.genres:
-                return 'Platform'
-            elif 'FPS' in dosGame.genres or 'First Person Shooter' in dosGame.genres:
-                return 'Gun-FPS'
-            elif 'Fighting' in dosGame.genres or 'Beat \'em Up' in dosGame.genres:
-                return 'BeatEmUp'
-            elif 'Strategy' in dosGame.genres and "Puzzle" not in dosGame.genres:
-                return 'Strategy-Gestion'
-            elif 'RPG' in dosGame.genres or 'Role-Playing' in dosGame.genres:
-                return 'RPG'
-            elif 'Interactive Fiction' in dosGame.genres:
-                return "Adventure-Visual"
-            elif "Adventure" in dosGame.genres and "Action" in dosGame.genres:
-                return "Action-Adventure"
-            elif "Adventure" in dosGame.genres or "Visual Novel" in dosGame.genres:
-                return "Adventure-Visual"
-            elif 'Simulation' in dosGame.genres and 'Managerial' in dosGame.genres:
-                return 'Strategy-Gestion'
-            elif 'Construction and Management Simulation' in dosGame.genres:
-                return 'Strategy-Gestion'
-            elif 'Simulation' in dosGame.genres:
-                return 'Simulation'
-            elif 'Shooter' in dosGame.genres:
-                return 'ShootEmUp'
-            elif 'Action' in dosGame.genres:
-                return 'Action-Adventure'
-            elif 'Arcade' in dosGame.genres or 'Life Simulation' in dosGame.genres:
-                return 'Misc'
-            elif 'Creativity' in dosGame.genres or 'App' in dosGame.genres or 'Reference' in dosGame.genres:
-                return 'Tools'
-            else:
-                return 'Unknown'
-        else:
-            return 'Unknown'
+        
+        if dosGame is None or dosGame.genres is None:
+            return GameGenres.UNKNOWN.value
+        
+        # list unique genres and sort them - precedence is alphabetical (e.g. "Managerial" + "Simulation" -> "Managerial")
+        genres = [g.strip() for g in list(set(dosGame.genres))]
+        genres.sort()
+        
+        # added handling for special cases
+        if 'Aventure' in genres and 'Action' in genres:
+            return GameGenres.ACTION_ADVENTURE.value
+        
+        if 'Education' in genres or 'Quiz' in genres:
+            if 'Adventure' in genres or 'Visual Novel' in genres:
+                return GameGenres.ACTION_ADVENTURE.value
+            return GameGenres.MISC.value
+        
+        # if not in special cases, return the first mapped genre found
+        for genre in genres:
+            output = GAME_MAPPER.get(genre, GameGenres.UNKNOWN)
+            if output != GameGenres.UNKNOWN:
+                return output.value
+        
+        # fallback - probably no genres defined
+        return GameGenres.UNKNOWN.value
+            
+
