81c47ea934ffe242bbf88a3d8cf28292c253da96
Felix Ruzzoli <felix.ruzzoli@lrx.gmbh>
2021-06-30 08:40:26 +0200

    [IMP] Extract list with games that should have run.bat handled

---
+++ lists.py
@@ -0,0 +1,16 @@
+handle_run_bat_for = [
+    'bisle2',
+    'Blood',
+    'Carmaged',
+    'comcon',
+    'comconra',
+    'CrypticP',
+    'lemm3',
+    'LewLeon',
+    'MechW2',
+    'rarkani1',
+    'Resurrec',
+    'stjudgec',
+    'heromm2d',
+    'longbow'
+]
